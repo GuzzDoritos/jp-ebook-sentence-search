e26b025d2ae0856ef999e02d4288b1723e75ad95
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-05-01 15:32:54 -0300

    Clean up extractor

--- ebook_extractor.py
+++ ebook_extractor.py
@@ -13,28 +13,6 @@ def get_tokenizer():
 
 tokenizer_obj = get_tokenizer()
 
-def main():
-    f = input("Type name of file: ")
-
-    try:
-        book = epub.read_epub(f)
-    except FileNotFoundError:
-        print(f"Error: File '{f}' was not found.")
-        return
-    except epub.EpubException as e:
-        print(f"Epub Error: {e}. Possibly tried to load non-compatible file?")
-        return
-
-    sentences = get_sentences(book)
-
-    print("Indexing file...")
-
-    index = create_index(sentences)
-
-    print("Found words:", len(index))
-
-    search_loop(index)
-
 def get_sentences(book):
     sentences = []
 
@@ -64,32 +42,6 @@ def create_index(sentences):
                     index[token] = set()
                 index[token].add(s)
     return index
-
-def search_loop(index):
-    word = input("Type a word: ")
-    while word:
-        tokens = tokenize(word)
-        if not tokens:
-            print("Could not tokenize input.")
-            word = input("Type a word: ")
-            continue
-        w = tokens[0]
-        if w in index:
-            for s in index[w]:
-                t_sentence = tokenizer_obj.tokenize(s, SplitMode.C)
-                target_list = []
-                for t in t_sentence:
-                    if t.dictionary_form() == w:
-                        target_list.append(t.surface())
-                
-                highlighted_text = s
-
-                for tw in target_list:
-                    highlighted_text = highlighted_text.replace(tw, f"{'\033[96m'}{tw}{'\033[0m'}")
-                print(highlighted_text, '\n')
-        else:
-            print("Word not found.")
-        word = input("Type a word: ")
 
 def has_japanese(text):
     # This pattern covers Hiragana, Katakana, and the main Kanji block
